09c22660593399000238731de348fc517c88da87
hhwe <hhw12125@gmail.com>
2018-09-12 20:18:29 +0800

    message queue and celery

--- huawei/count_str.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-
-s = sys.stdin.readline().lower()
-w, d = (s.split())
-print(d)
-print(w.count(d))
--- huawei/last_word.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-
-r = sys.stdin.readline()
-l = r.split()[-1]
-print(l)
-print(len(l))
