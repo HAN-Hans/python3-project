64cc2d32ee56884bee3d0a3108ef17c69a2728e7
hhwe <hhw12125@gmail.com>
2018-09-14 18:13:10 +0800

    niuke huawei online testing

--- oj/huawei.py
+++ oj/huawei.py
@@ -356,26 +356,93 @@
 #         break
 
 
-from collections import namedtuple
+# from collections import namedtuple
+# while True:
+#     try:
+#         n = int(input())
+#         a = [int(i) for i in input().strip().split()]
+#         l, m = [0 for i in range(n)], [0 for i in range(n)]
+#         l[0], m[n-1] = 1, 1
+#         for i in range(1, n):
+#             l[i], m[n-1-i] = 1, 1
+#             for j in range(i):
+#                 # 如果大于前面的数字, 只需要查看该值的最长子序列
+#                 # 如果大于当前当前就将当前值的设置为前序加一
+#                 if a[i] > a[j] and l[j] + 1 > l[i]:
+#                     l[i] = l[j] + 1
+#                 if a[n-1-i] > a[n-1-j] and m[n-1-i] + 1 > m[n-1-j]:
+#                     m[n-1-i] = m[n-1-j] + 1
+#         ans = 0
+#         for i in range(n):
+#             if (l[i] + m[i] - 1 > ans):
+#                 ans = l[i] + m[i] - 1
+#         print(n - ans)
+#     except:
+#         break
+
+
+# 22
+'''
+
+﻿
+输入描述:
+﻿一组输入整数序列I和一组规则整数序列R，I和R序列的第一个整数为序列的个数（个数不包含第一个整数）；
+整数范围为0~0xFFFFFFFF，序列个数不限
+输出描述:
+﻿从R依次中取出R<i>，对I进行处理，找到满足条件的I<j>： 
+I<j>整数对应的数字需要连续包含R<i>对应的数字。比如R<i>为23，I<j>为231，那么I<j>包含了R<i>，条件满足 。 
+按R<i>从小到大的顺序:
+(1)先输出R<i>； 
+(2)再输出满足条件的I<j>的个数； 
+(3)然后输出满足条件的I<j>在I序列中的位置索引(从0开始)； 
+(4)最后再输出I<j>。 
+附加条件： 
+(1)R<i>需要从小到大排序。相同的R<i>只需要输出索引小的以及满足条件的I<j>，索引大的需要过滤掉 
+(2)如果没有满足条件的I<j>，对应的R<i>不用输出 
+(3)最后需要在输出序列的第一个整数位置记录后续整数序列的个数(不包含“个数”本身)
+
+序列I：15,123,456,786,453,46,7,5,3,665,453456,745,456,786,453,123（第一个15表明后续有15个整数） 
+序列R：5,6,3,6,3,0（第一个5表明后续有5个整数） 
+输出：30, 3,6,0,123,3,453,7,3,9,453456,13,453,14,123,6,7,1,456,2,786,4,46,8,665,9,453456,11,456,12,786
+说明：
+30----后续有30个整数
+3----从小到大排序，第一个R<i>为0，但没有满足条件的I<j>，不输出0，而下一个R<i>是3
+6--- 存在6个包含3的I<j> 
+0--- 123所在的原序号为0 
+123--- 123包含3，满足条件 
+示例1
+输入
+复制
+15 123 456 786 453 46 7 5 3 665 453456 745 456 786 453 123 
+5 6 3 6 3 0
+输出
+复制
+30 3 6 0 123 3 453 7 3 9 453456 13 453 14 123 6 7 1 456 2 786 4 46 8 665 9 453456 11 456 12 786
+'''
 while True:
     try:
-        n = int(input())
-        a = [int(i) for i in input().strip().split()]
-        l, m = [0 for i in range(n)], [0 for i in range(n)]
-        l[0], m[n-1] = 1, 1
-        for i in range(1, n):
-            l[i], m[n-1-i] = 1, 1
-            for j in range(i):
-                # 如果大于前面的数字, 只需要查看该值的最长子序列
-                # 如果大于当前当前就将当前值的设置为前序加一
-                if a[i] > a[j] and l[j] + 1 > l[i]:
-                    l[i] = l[j] + 1
-                if a[n-1-i] > a[n-1-j] and m[n-1-i] + 1 > m[n-1-j]:
-                    m[n-1-i] = m[n-1-j] + 1
-        ans = 0
-        for i in range(n):
-            if (l[i] + m[i] - 1 > ans):
-                ans = l[i] + m[i] - 1
-        print(n - ans)
+        a = input().split()
+        n, p = int(a[0]), a[1:]
+        b = input().split()
+        m, q = int(b[0]), sorted(set(b[1:]))
+        r, s = [], []
+        # print(n, p, m, q)
+        for i in range(len(q)):
+            res = []
+            for j in range(n):
+                if q[i] in p[j]:
+                    res.extend([j, p[j]])
+                    if q[i] not in s:
+                        s.append(q[i])
+            if res:
+                r.append(res)
+        l = 0
+        p = [0]
+        for i in range(len(r)):
+            p.append(s[i])
+            p.append(len(r[i])//2)
+            p.extend(r[i])
+        p[0] = len(p) -1
+        print(' '.join(p))
     except:
         break
